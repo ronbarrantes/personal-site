8a4ac2d21c91fc8d8cbccf8a312c40d20b5b3584
ronbarrantes <ronbarrantes@gmail.com>
2021-01-27 17:33:55 -0800

    Add a little emotion to the nav

--- src/components/Menu.tsx
+++ src/components/Menu.tsx
@@ -1,10 +1,16 @@
 import React from 'react'
 import styled from '@emotion/styled'
-
 import { Constants } from '../content/constants'
 import { Link } from 'react-router-dom'
 
-export const StyledNavLink = styled(Link)``
+const color = 'darkgreen'
+
+export const StyledNavLink = styled(Link)`
+  background-color: hotpink;
+  &:hover {
+    color: ${color};
+  }
+`
 
 const { Gallery, Email } = Constants.Menu
 
