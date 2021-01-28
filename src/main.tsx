bd092097a0dd1e532379b0bd2ac2b7775b1688ed
ronbarrantes <ronbarrantes@gmail.com>
2021-01-27 16:12:02 -0800

    Add RouterDom to main

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,7 @@
 import 'reset-css'
 import React from 'react'
 import ReactDOM from 'react-dom'
+import { BrowserRouter as Router } from 'react-router-dom'
 import App from './components/App'
 
 const container = document.createElement('div')
@@ -10,4 +11,8 @@ const rootElement = document.createElement('div')
 rootElement.className = 'root'
 document.body.appendChild(rootElement)
 
-ReactDOM.render(<App />, rootElement)
+ReactDOM.render(
+  <Router>
+    <App />
+  </Router>
+  , rootElement)
