f7466423b168ee6839a6fb67acfd5d2f9ec37355
ronbarrantes <ronbarrantes@gmail.com>
2022-01-20 15:21:11 -0800

    Got the app to work

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,7 @@
 import * as React from 'react'
 import * as ReactDOM from 'react-dom'
-// import { hot } from 'react-hot-loader/root'
-
-// const App = ({ name }: {name?: string}) => <div>Hello {name}</div>
+import App from './components/App'
 
 const root = document.createElement('root')
-
-root.innerHTML = '<div>hello :)</div>'
-
-ReactDOM.render(<div>Hello world</div>, root)
 document.body.appendChild(root)
+ReactDOM.render(<App name="Ron" />, root)
