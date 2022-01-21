01253d2f8b138934f5911b78756388ae3bcaadec
ronbarrantes <ronbarrantes@gmail.com>
2022-01-21 14:48:55 -0800

    Add stuff to the src

--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,18 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-
 import App from './components/App'
+import './styles/reset.css'
+import './styles/styles.css'
 
 const root = document.createElement('root')
+const portalEl = document.createElement('div')
+portalEl.id = 'modal'
+
+document.body.appendChild(portalEl)
 document.body.appendChild(root)
-ReactDOM.render(<App name="Ron" />, root)
+
+ReactDOM.render(
+	<React.StrictMode>
+		<App />
+	</React.StrictMode>
+, root)
