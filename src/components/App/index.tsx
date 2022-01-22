418b73193ae5345ecc7aae20d56d6ab54ebaeba6
ronbarrantes <ronbarrantes@gmail.com>
2022-01-21 23:31:39 -0800

    Add experience and footer

--- src/components/App/index.tsx
+++ src/components/App/index.tsx
@@ -4,10 +4,10 @@ import './styles.css'
 
 import About from '../About'
 // import Contact from '../Contact';
-// import Footer from '../Footer';
+import Footer from '../Footer'
 import Header from '../Header'
 // import Portfolio from '../Portfolio';
-// import Experience from '../Experience';
+import Experience from '../Experience'
 // import { PortfolioProvider } from '../../hooks/usePortfolio'
 
 function App() {
@@ -17,10 +17,10 @@ function App() {
       <About />
       {/* <PortfolioProvider>
         <Portfolio />
-      </PortfolioProvider>
+      </PortfolioProvider> */}
       <Experience />
-      <Contact />
-      <Footer /> */}
+      {/* <Contact /> */}
+      <Footer />
     </div>
   )
 }
