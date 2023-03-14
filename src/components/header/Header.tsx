379ee6c96d9c990c166d4a4baa9b61f96825fb2e
ronbarrantes <ronbarrantes@gmail.com>
2023-03-14 11:39:52 -0700

    Use hook on header for dev purposes

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,10 +1,18 @@
 import { Logo } from '../branding/Logo'
+import classNames from 'classnames'
+import { useScreenSize } from '@/client-data/hooks/use-screen-size'
 
 const Header = () => {
+  const { width, height } = useScreenSize()
+
   return (
-    <header className="flex flex-col items-center justify-center border border-red-500">
+    <header className="relative flex flex-col items-center justify-center border border-red-500">
+      <div className="text-small absolute top-0 left-0 text-green-400">
+        {`${width}px | ${height}px`}
+      </div>
       <h1 className="text-5xl font-bold">{`Ron's site 😊`}</h1>
-      <Logo />
+
+      {/* <Logo /> */}
     </header>
   )
 }
