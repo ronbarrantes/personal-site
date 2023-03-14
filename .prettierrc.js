dba9e870edb6a3ebc90209132b2efd0ebc05b80b
ronbarrantes <ronbarrantes@gmail.com>
2023-03-14 13:33:32 -0700

    Sort imports

--- .prettierrc.js
+++ .prettierrc.js
@@ -1,17 +1,18 @@
 module.exports = {
-  semi: true,
+  semi: false,
   singleQuote: true,
   jsxSingleQuote: false,
   tabWidth: 2,
   useTabs: false,
-
   importOrder: [
-    "^(next/(.*)$)|^(next$)",
-    "<THIRD_PARTY_MODULES>",
-    "^@components/(.*)$",
-    "^@/(.*)$",
-    "^[./]",
+    '^(next/(.*)$)|^(next$)',
+    '^react',
+
+    '<THIRD_PARTY_MODULES>',
+    '^@components/(.*)$',
+    '^@/(.*)$',
+    '^[./]',
   ],
   importOrderSeparation: true,
   importOrderSortSpecifiers: true,
-};
+}
