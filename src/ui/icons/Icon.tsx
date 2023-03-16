7c1017018ce6abeee976a619396e0cc646dc80fe
ronbarrantes <ronbarrantes@gmail.com>
2023-03-16 02:06:25 -0700

    Add types

---
+++ src/ui/icons/Icon.tsx
@@ -0,0 +1,9 @@
+import { Icons as IconProps } from './icons'
+
+type Props =
+  | { name: 'fluentui'; color?: string }
+  | { name: 'fluentui'; color?: string }
+
+export const Icon = (props: IconProps) => {
+  return <Icon></Icon>
+}
---
+++ src/ui/icons/icons.d.ts
@@ -0,0 +1,34 @@
+type IconPattern = {
+  name: string
+  file: string
+}
+
+type IconsArr =
+  | { name: 'azure'; file: 'Azure' }
+  | { name: 'css'; file: 'Css' }
+  | { name: 'fluent'; file: 'FluentUi' }
+  | { name: 'html'; file: 'Html' }
+  | { name: 'illustrator'; file: 'Illustrator' }
+  | { name: 'indesign'; file: 'InDesign' }
+  | { name: 'javascript'; file: 'JavaScript' }
+  | { name: 'jest'; file: 'Jest' }
+  | { name: 'knockout'; file: 'Knockout' }
+  | { name: 'microsoft'; file: 'Microsoft' }
+  | { name: 'mochajs'; file: 'MochaJs' }
+  | { name: 'mysql'; file: 'MySql' }
+  | { name: 'nextjs'; file: 'NextJs' }
+  | { name: 'planetscale'; file: 'PlanetScale' }
+  | { name: 'postgresql'; file: 'PostgreSql' }
+  | { name: 'premiere'; file: 'PremierePro' }
+  | { name: 'rails'; file: 'Rails' }
+  | { name: 'react'; file: 'React' }
+  | { name: 'redis'; file: 'Redis' }
+  | { name: 'redux'; file: 'Redux' }
+  | { name: 'retool'; file: 'Retool' }
+  | { name: 'selenium'; file: 'Selenium' }
+  | { name: 'stripe'; file: 'Stripe' }
+  | { name: 'trpc'; file: 'Trpc' }
+  | { name: 'turborepo'; file: 'TurboRepo' }
+  | { name: 'typescript'; file: 'TypeScript' }
+  | { name: 'vercel'; file: 'Vercel' }
+  | { name: 'worldress'; file: 'Wordpress' }
