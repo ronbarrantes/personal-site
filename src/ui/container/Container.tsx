ee142ef8e69936a9d8ee52a0445be365e06f709d
ronbarrantes <ronbarrantes@gmail.com>
2023-03-20 22:18:37 -0700

    Change wrapper to container

---
+++ src/ui/container/Container.tsx
@@ -0,0 +1,30 @@
+import classNames from 'classnames'
+
+interface ContainerProps {
+  title?: string
+  description?: string
+  className?: string | string[]
+  children: React.ReactNode
+}
+
+export const Container = ({
+  title,
+  description,
+  children,
+  className,
+}: ContainerProps) => {
+  return (
+    <div className={classNames('mx-auto max-w-7xl', className)}>
+      {title && <h2 className="text-3xl font-semibold">{title}</h2>}
+      {description &&
+        (Array.isArray(description) ? (
+          description.map((item, idx) => {
+            return <p key={`${item}-${idx}`}>{item}</p>
+          })
+        ) : (
+          <p>{description}</p>
+        ))}
+      <>{children}</>
+    </div>
+  )
+}
--- src/ui/index.tsx
+++ src/ui/index.tsx
@@ -1,6 +1,6 @@
 export { Badge } from './badge/Badge'
+export { Container } from './container/Container'
 export { Icon } from './icon/Icon'
 export { type IconsLisType, iconsLisFiles } from './icon/icon-types'
 export { Logo } from './logo/Logo'
 export { Tooltip } from './radix/tooltip/Tooltip'
-export { Wrapper } from './wrapper/Wrapper'
