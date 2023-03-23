447b09a411e136a3187da8e177d9a84ac58284d9
Ron Barrantes <ronbarrantes@gmail.com>
2023-03-23 14:47:03 -0700

    update header (#43)
    * Fix header
    
    * Fix stuff

--- src/components/app/header/Header.tsx
+++ src/components/app/header/Header.tsx
@@ -6,12 +6,11 @@ import { Nav } from '../nav/Nav'
 
 const Header = () => {
   return (
-    <header className="relative bg-neutral-800">
-      <Container className="flex !flex-row items-center justify-between border border-transparent border-b-neutral-600 !py-0 md:px-16">
+    <header className="relative border border-transparent border-b-neutral-700 bg-neutral-800">
+      <Container className="flex !flex-row items-center justify-between  !py-0 md:px-16">
         <Link href="/" aria-label="home page" className="w-fit p-2">
-          <Logo className="h-10 w-fit fill-neutral-100" />
+          <Logo className="h-10 w-fit fill-neutral-200" />
         </Link>
-
         {menu.length > 1 && <Nav menu={menu} />}
       </Container>
     </header>
