e3bfb0a782793462dbb46649ac319b263d99e149
ronbarrantes <ronbarrantes@gmail.com>
2023-03-23 14:28:00 -0700

    Update the header

--- src/components/app/header/Header.tsx
+++ src/components/app/header/Header.tsx
@@ -7,7 +7,7 @@ import { Nav } from '../nav/Nav'
 const Header = () => {
   return (
     <header className="relative bg-neutral-800">
-      <Container className="flex !flex-row items-center justify-between py-0 md:px-16">
+      <Container className="flex !flex-row items-center justify-between border border-transparent border-b-neutral-600 !py-0 md:px-16">
         <Link href="/" aria-label="home page" className="w-fit p-2">
           <Logo className="h-10 w-fit fill-neutral-100" />
         </Link>
