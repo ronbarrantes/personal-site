64cc1d0ad57c579ffab85e27d793465ec145ffdd
ronbarrantes <ronbarrantes@gmail.com>
2023-03-23 14:44:13 -0700

    Fix header

--- src/components/app/header/Header.tsx
+++ src/components/app/header/Header.tsx
@@ -1,17 +1,16 @@
 import Link from 'next/link'
 
 import { Container, Logo } from '@ui'
+import { Nav } from '@components/app/nav/Nav'
 import { menu } from '@/client-data/data/nav-menu'
-import { Nav } from '../nav/Nav'
 
 const Header = () => {
   return (
     <header className="relative bg-neutral-800">
-      <Container className="flex !flex-row items-center justify-between border border-transparent border-b-neutral-600 !py-0 md:px-16">
+      <Container className="flex !flex-row items-center justify-between !py-0 md:px-16">
         <Link href="/" aria-label="home page" className="w-fit p-2">
-          <Logo className="h-10 w-fit fill-neutral-100" />
+          <Logo className="h-10 w-fit fill-purple-200" />
         </Link>
-
         {menu.length > 1 && <Nav menu={menu} />}
       </Container>
     </header>
