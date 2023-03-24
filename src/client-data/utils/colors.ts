42a6efad0a80265a50ac6dbf1d0eb4879bb12158
ronbarrantes <ronbarrantes@gmail.com>
2023-03-24 13:11:51 -0700

    Add a bunch of color functions

--- src/client-data/utils/colors.ts
+++ src/client-data/utils/colors.ts
@@ -8,10 +8,81 @@ export const hexToRGB = (hex: string, asArray?: boolean) => {
   const b = parseInt(hex.slice(5, 7), 16)
 
   if (asArray) return [r, g, b]
-
   return `${r}, ${g}, ${b}`
 }
 
-// FIND THE CLOSEST COLOR FROM AN ARRAY OF COLORS
+export const brightnessFormula = (color: number[]): number =>
+  Math.sqrt(
+    color[0] * color[0] * 0.241 +
+      color[0] * color[0] * 0.691 +
+      color[0] * color[0] * 0.068
+  )
 
-// https://stackoverflow.com/questions/9733288/how-to-programmatically-calculate-the-contrast-ratio-between-two-colors
+export const isColorTooDark = (colorString: string): boolean => {
+  if (colorString.length !== 3 && colorString.length !== 6)
+    throw new Error('Not the right size for a color')
+  let hex = colorString.split('')
+  if (hex.length === 3) hex = hex.map((val) => val + val)
+  else {
+    const newHex = []
+    for (let i = 0; i < hex.length; i += 2) {
+      newHex.push(hex[i] + hex[i + 1])
+    }
+    hex = newHex
+  }
+  const dec = hex.map((val) => parseInt(val, 16))
+  return brightnessFormula(dec) < 130
+}
+
+const closestColors = [
+  {
+    input: '#000000',
+    array: ['#ffffff', '#000000', '#ff0000'],
+    output: '#000000',
+  },
+  {
+    input: '#ffffff',
+    array: ['#ffffff', '#000000', '#ff0000'],
+    output: '#ffffff',
+  },
+  {
+    input: '#e60f0f',
+    array: ['#ffffff', '#000000', '#ff0000'],
+    output: '#ff0000',
+  },
+  {
+    input: '#15f200',
+    array: ['#ffffff', '#000000', '#00ff00'],
+    output: '#00ff00',
+  },
+]
+
+//distance = sqrt((r2-r1)^2 + (g2-g1)^2 + (b2-b1)^2)
+// weighed distance = d = sqrt(((r2-r1)*0.3)^2 + ((g2-g1)*0.59)^2 + ((b2-b1)*0.11)^2)
+
+const closestColor = (color: string, colors: string[]) => {
+  const colorRGB = hexToRGB(color, true) as number[]
+
+  const distanceList = colors.map((currColor) => {
+    const currColorRGB = hexToRGB(currColor, true) as number[]
+    return Math.sqrt(
+      Math.pow(currColorRGB[0] - colorRGB[0], 2) +
+        Math.pow(currColorRGB[1] - colorRGB[1], 2) +
+        Math.pow(currColorRGB[2] - colorRGB[2], 2)
+    )
+  })
+
+  const closestColorIndex = distanceList.indexOf(Math.min(...distanceList))
+
+  return colors[closestColorIndex]
+}
+
+closestColors.forEach((test, idx) => {
+  const result = closestColor(test.input, test.array)
+
+  if (result !== test.output) {
+    console.log('Failed test', idx, test)
+  }
+
+  console.log(idx, test.input, '|', result)
+})
