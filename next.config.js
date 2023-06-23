96d759365bf57664949fa04c1107caa0a65501cc
ronbarrantes <ronbarrantes@gmail.com>
2023-06-23 00:49:34 -0700

    Change experimental flag

--- next.config.js
+++ next.config.js
@@ -2,6 +2,10 @@
 const { withContentlayer } = require('next-contentlayer')
 
 /** @type {import('next').NextConfig} */
-const nextConfig = { reactStrictMode: true, swcMinify: true }
+const nextConfig = {
+  reactStrictMode: true,
+  swcMinify: true,
+  experimental: { appDir: false },
+}
 
 module.exports = withContentlayer(nextConfig)
