ada4758386d674deac22f147a57da7508584cdb2
Ron Barrantes <ronbarrantes@gmail.com>
2023-06-23 01:31:32 -0700

    Adding a content layer (#52)
    * Add content-layer files
    
    * Add some config stuff
    
    * Update the first post
    
    * Update configuration lock file
    
    * Change experimental flag
    
    * Add external flag
    
    * Try diff configuration
    
    * Add more config

--- next.config.js
+++ next.config.js
@@ -1,6 +1,14 @@
+const { withContentlayer } = require('next-contentlayer')
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  swcMinify: true,
+
+  experimental: {
+    appDir: false,
+    // externalDir: true
+  },
 }
 
-module.exports = nextConfig
+module.exports = withContentlayer(nextConfig)
