f19cc3653ba1752a2ed0bef14932a92efbc5a219
ronbarrantes <ronbarrantes@gmail.com>
2023-07-03 11:59:15 -0700

    Add filePathPattern

--- contentlayer.config.ts
+++ contentlayer.config.ts
@@ -2,7 +2,8 @@ import { defineDocumentType, makeSource } from 'contentlayer/source-files'
 
 export const Post = defineDocumentType(() => ({
   name: 'Post',
-  filePathPattern: `**/*.md`,
+  // filePathPattern: `**/*.md`,
+  filePathPattern: `posts/**/*.md`,
   fields: {
     title: { type: 'string', required: true },
     date: { type: 'date', required: true },
