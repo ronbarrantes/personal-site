b42563473eaa2b29425ba5ed3bb9ae56a1c140f4
Ron Barrantes <ronbarrantes@gmail.com>
2026-04-24 21:47:29 -0400

    Remove the old designs

--- frontend/src/utils/router.ts
+++ frontend/src/utils/router.ts
@@ -2,12 +2,6 @@ import { createBrowserRouter } from "react-router";
 
 import { About } from "@/pages/About";
 import { Contact } from "@/pages/Contact";
-import { DesignBrutalist } from "@/pages/designs/DesignBrutalist";
-import { DesignDeco } from "@/pages/designs/DesignDeco";
-import { DesignEditorial } from "@/pages/designs/DesignEditorial";
-import { DesignOrganic } from "@/pages/designs/DesignOrganic";
-import { DesignsIndex } from "@/pages/designs/DesignsIndex";
-import { DesignTerminal } from "@/pages/designs/DesignTerminal";
 import { MainLogin } from "@/pages/MainLogin";
 import { MainPage } from "@/pages/MainPage";
 import { Portfolio } from "@/pages/Portfolio";
@@ -26,10 +20,4 @@ export const router = createBrowserRouter([
       { path: "/contact", Component: Contact },
     ],
   },
-  { path: "/designs", Component: DesignsIndex },
-  { path: "/designs/terminal", Component: DesignTerminal },
-  { path: "/designs/editorial", Component: DesignEditorial },
-  { path: "/designs/brutalist", Component: DesignBrutalist },
-  { path: "/designs/deco", Component: DesignDeco },
-  { path: "/designs/organic", Component: DesignOrganic },
 ]);
