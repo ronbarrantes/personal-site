37f824c221306fae99be22299a6ce4169fb6d009
Ron Barrantes <ronbarrantes@gmail.com>
2026-06-02 18:32:03 -0400

    Add correct favicon

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -9,6 +9,10 @@ import "@/index.css";
 export const metadata: Metadata = {
   title: "RON/B.CO",
   description: "Personal site for Ron Barrantes",
+  icons: {
+    icon: "/favicon.png",
+    apple: "/favicon-144.png",
+  },
 };
 
 export default function RootLayout({ children }: Readonly<{ children: ReactNode }>) {
